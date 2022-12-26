87791d4d15477a4e3877715b18347e5fd009e283
risaadodx <wrisaad@gmail.com>
2022-12-26 18:46:26 +0600

    database connected

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@dbuser1.oi3ncct.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const homesCollection = client.db("shelterGuruDb").collection("homes");
+
+    console.log("Database Connected...");
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
