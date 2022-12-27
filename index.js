9d8795dae300708e7922f7bc7a53f11a9a4f9bc8
risaadodx <wrisaad@gmail.com>
2022-12-27 15:01:05 +0600

    save user email & generate jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,6 +21,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const homesCollection = client.db("shelterGuruDb").collection("homes");
+    const usersCollection = client.db("shelterGuruDb").collection("users");
+
+    //save user email & generate jwt
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      console.log(token);
+      res.send({ result, token });
+    });
 
     console.log("Database Connected...");
   } finally {
